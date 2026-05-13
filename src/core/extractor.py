3ff03f45baa1ab9b9bace5e9a75da8248e99e27c
WordlessMeteor <1925009271@qq.com>
2026-05-13 12:40:39 +0800

    荣誉嘉宾默认名称问题修复【Default GoH Name BUG Fix】
    修复了游戏数据提取脚本的一个问题，该问题曾导致在获取互斥荣誉嘉宾的本地化名称时，如果没有在字符串常量池中找到相应的键，那么会默认显示一个不正确的键。
    Fixed a BUG in Game Data Extraction Program that when the program tried to got the localized name of a mutex GoH, if the localization key wasn't present in stringtable, then an incorrect default value would be displayed.

--- src/core/extractor.py
+++ src/core/extractor.py
@@ -27,7 +27,7 @@ args = parser.parse_args()
 # 作者（Author）：          WordlessMeteor
 # 主页（Home page）：       https://github.com/WordlessMeteor/LoL-DIY-Programs/
 # 鸣谢（Acknowledgement）： Morilli, Le poussin, Moga
-# 更新（Last update）：     2026/05/12
+# 更新（Last update）：     2026/05/13
 #=============================================================================
 
 warnings.simplefilter("error") #在数据提取器基类的变量代换方法中使用`eval`函数对装备说明文本中的变量进行预计算时，会出现大量`<string>:1: SyntaxWarning: 'int' object is not callable; perhaps you missed a comma?`的警告信息。这是因为之前在处理模式分化数值时，会出现形如“@{var}@ (mode: {mode})”的表达式。虽然不可计算，但是在`eval`处理的过程中发出了警告。通过这一条命令，强制本程序不允许任何警告——警告即报错（When `LoLDataExtractor.variableSubstitution` method pre-calculates variables in item tooltips using `eval` function, a lot of warnings like `<string>:1: SyntaxWarning: 'int' object is not callable; perhaps you missed a comma?` will pop up. This is because when the program handles mode specific data values earlier, expressions in the form of "@{var}@ (mode: {mode})" exist. Although it can't be calculated, a warning is thrown anyway when `eval` function parses the string. By this command, no warnings are allowed in this program - all warnings will be raised as errors）
@@ -7059,7 +7059,7 @@ class GoHExtractor(LoLDataExtractor):
                             if GoH_key in self.map30_bin:
                                 GoH_name: str = self.map30_bin[GoH_key]["name"]
                                 GoH_subtitle_key: str = GoH_cherry[GoH_name]["{1ff99d7f}"]["Subtitle"]
-                                GoH_subtitle: str = self.get_strtable_value(strtable_locale, GoH_subtitle_key, default = tooltip_key)
+                                GoH_subtitle: str = self.get_strtable_value(strtable_locale, GoH_subtitle_key, default = GoH_subtitle_key)
                                 mutexGoHNames.append(GoH_subtitle)
                             else:
                                 mutexGoHNames.append("")
