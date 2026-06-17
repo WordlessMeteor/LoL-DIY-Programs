84d40a3e9a18067db3ccaa252e3fd8a4183bc405
WordlessMeteor <1925009271@qq.com>
2026-06-18 00:16:45 +0800

    对局信息文件备份目录优化【Directory Optimization of Match Information File Backup】
    现在，对局信息存档脚本允许用户选择压缩包生成的目录，而不再总是生成到当前目录。
    Now, Match Information Archive Program allows users to select a folder to store the archive, instead of always generating it into the current working directory.

--- 存档对局信息文件.py
+++ 存档对局信息文件.py
@@ -1,6 +1,22 @@
 import os, subprocess, time
 
+print("请指定压缩后形成的文件的存放目录：\nPlease specify the directory to store the archive:")
+while True:
+    folder: str = input()
+    if folder == "":
+        continue
+    elif folder == chr(4):
+        exit(1)
+    elif not os.path.exists(folder):
+        print("您输入的路径不存在。请重新输入。\nPath not found. Please try again.")
+    elif not os.path.isdir(folder):
+        print("请输入一个文件夹。\nPlease input a folder.")
+    else:
+        folder = folder.replace("/", "\\")
+        break
 #获取待压缩的文件夹列表（Get the list of folders to compress）
+print("[%s]" %(time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())), end = "")
+print("正在整理文件列表……\nSorting out the file list ...")
 matchInfo_folders_to_compress: list[str] = []
 matchId_folder_name: str = "1. MatchIDs"
 for root, dirs, files in os.walk("召唤师信息（Summoner Information）"):
@@ -11,7 +27,7 @@ currentTime: str = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
 print("[%s]" %(currentTime), end = "")
 print("正在压缩对局信息文件……\nCompressing match information files ...")
 archive_name: str = f"对局信息存档 {currentTime}.7z"
-args: list[str] = ["bandizip", "c", "-storeroot:yes", "-l:9", archive_name, *matchInfo_folders_to_compress]
+args: list[str] = ["bandizip", "c", "-storeroot:yes", "-l:9", os.path.join(folder, archive_name), *matchInfo_folders_to_compress]
 result: subprocess.CompletedProcess[bytes] = subprocess.run(args)
 print("[%s]" %(time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())), end = "")
 if result.returncode == 0:
