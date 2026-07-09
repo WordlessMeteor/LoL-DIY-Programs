e4c9f21a06cda5dba23155b9d41d39b9a37ac4e3
WordlessMeteor <1925009271@qq.com>
2026-07-10 02:35:55 +0800

    更新游戏队列数据结构【Update Game Queue Data Structure】
    1. 添加了一个“团队语音可用性”字段，并移动了“英雄选择模式”字段的位置。（表头模块全部改动，游戏模式模块第26、28、35～37、39、40、45、46、48、50和57行和自定义脚本主数据框结构工作簿`queue_header`工作表第32和40行。）
    Added a "teamVoiceEnabled" field and moved the location of "pickMode" field. (All changes in Header Module, Lines 26, 28, 35～37, 39, 40, 45, 46, 48, 50 and 57 in Game Mode Module and Lines 32 and 40 in Sheet `queue_header` in Customized Program Main Dataframe Structure Workbook.)
    2. 优化了一处代码：对于具体的某个表头，使用该表头字符串而不是一律用`key`作为字典的下标。（游戏模式模块第28、30、32、34、36、38和47行。）
    Optimized a piece of code: For some specific header, use this key string instead of the general `key` as the subscript of the dictionary. (Lines 28, 30, 32, 34, 36, 38 and 47 in Game Mode Module.)

--- src/core/dataframes/gameMode.py
+++ src/core/dataframes/gameMode.py
@@ -23,38 +23,38 @@ def sort_queue_data(queues: list[dict[str, Any]]) -> pandas.DataFrame:
     for queue in queues:
         for i in range(len(queue_header_keys)):
             key = queue_header_keys[i]
-            if i <= 41:
+            if i <= 43:
                 if i == 3: #对局类型（`category`）
-                    to_append: Any = categories[queue[key]]
+                    to_append: Any = categories[queue["category"]]
                 elif i == 8: #游戏选择类别（`gameSelectCategory`）
-                    to_append = gameSelectCategories[queue[key]]
+                    to_append = gameSelectCategories[queue["gameSelectCategory"]]
                 elif i == 9: #游戏模式分组（`gameSelectModeGroup`）
-                    to_append = gameSelectModeGroups[queue[key]]
+                    to_append = gameSelectModeGroups[queue["gameSelectModeGroup"]]
                 elif i == 25: #双排最高段位限制（`maxTierForPremadeSize2`）
-                    to_append = tiers[queue[key]]
-                elif i == 32: #队列可用性（`queueAvailability`）
-                    to_append = queueAvailability_dict[queue[key]]
-                elif i == 40 or i == 41: #上次关闭时间和上次开放时间（`lastToggledOffDate` and `lastToggledOnDate`）
-                    subkey = "lastToggledOffTime" if i == 40 else "lastToggledOnTime"
+                    to_append = tiers[queue["maxTierForPremadeSize2"]]
+                elif i == 32: #英雄选择模式（`pickMode`）
+                    to_append = pickModes[queue["pickMode"]]
+                elif i == 33: #队列可用性（`queueAvailability`）
+                    to_append = queueAvailability_dict[queue["queueAvailability"]]
+                elif i == 42 or i == 43: #上次关闭时间和上次开放时间（`lastToggledOffDate` and `lastToggledOnDate`）
+                    subkey = "lastToggledOffTime" if i == 42 else "lastToggledOnTime"
                     standard_time = time.strftime("%Y年%m月%d日%H:%M:%S", time.localtime(queue[subkey] / 1000))
                     to_append = standard_time
                 else:
                     to_append = queue[key]
-            elif i <= 63:
-                if i == 44: #禁用模式（`banMode`）
-                    to_append = banModes[queue["gameTypeConfig"][key]]
-                elif i == 53: #游戏类型序号（`typeId`）
+            elif i <= 64:
+                if i == 46: #禁用模式（`banMode`）
+                    to_append = banModes[queue["gameTypeConfig"]["banMode"]]
+                elif i == 55: #游戏类型序号（`typeId`）
                     to_append = queue["gameTypeConfig"]["id"]
-                elif i == 57: #英雄选择策略（`typeName`）
+                elif i == 59: #英雄选择策略（`typeName`）
                     to_append = queue["gameTypeConfig"]["name"]
-                elif i == 60: #英雄选择模式（`pickMode`）
-                    to_append = pickModes[queue["gameTypeConfig"][key]]
                 else:
                     to_append = queue["gameTypeConfig"][key]
             else:
                 to_append = queue["queueRewards"][key]
             queue_data[key].append(to_append)
-    queue_output_order: list[int] = [12, 32, 19, 15, 7, 29, 5, 6, 22, 53, 3, 39, 2, 8, 9, 10, 0, 57, 14, 16, 17, 44, 60, 40, 41, 30, 25, 23, 27, 4, 31, 28, 26, 56, 38, 24, 36, 37, 18, 51, 62, 46, 50, 1, 63, 47, 11, 45, 55, 61, 33, 34, 49, 54, 42, 43, 59, 48, 64, 65, 66, 13]
+    queue_output_order: list[int] = [12, 33, 19, 15, 7, 29, 5, 6, 22, 55, 3, 41, 2, 8, 9, 10, 0, 59, 14, 16, 17, 46, 32, 42, 43, 30, 25, 23, 27, 4, 31, 28, 26, 58, 39, 24, 37, 38, 18, 40, 53, 63, 48, 52, 1, 64, 49, 11, 47, 57, 62, 34, 35, 51, 56, 44, 45, 61, 50, 65, 66, 67, 13]
     queue_data_organized: dict[str, list[Any]] = {queue_header_keys[i]: queue_data[queue_header_keys[i]] for i in queue_output_order}
     queue_df: pandas.DataFrame = pandas.DataFrame(data = queue_data_organized)
     queue_df = queue_df.sort_values(by = "id", ascending = True, ignore_index = True)
