84421c8be14643e68dfcffaeeb6eb406c02bee33
WordlessMeteor <1925009271@qq.com>
2026-07-31 05:20:52 +0800

    海克斯大乱斗 经典模式版地图本地化完善【Localization Improvement of ARAM: Mayhem Classic-ish Map】
    1.3.0版本现已锁定。
    Version 1.3.0 is locked now.
    完善海克斯大乱斗 经典模式版地图的本地化名称。
    Improved the localized name of ARAM: Mayhem Classic-ish map.

--- src/core/dataframes/matchHistory.py
+++ src/core/dataframes/matchHistory.py
@@ -2158,7 +2158,7 @@ def generate_LoLHistory_records(LoLHistory_data: dict[str, list[Any]], LoLGame_s
                     elif "mapskin_ha_crepe" in LoLGame_summary["gameModeMutators"]:
                         mapName = "进步之桥"
                     elif "mapskin_map12_jade" in LoLGame_summary["gameModeMutators"]:
-                        mapName = "LCU_Map12_Name_Jade"
+                        mapName = "召唤师峡谷？"
                     else:
                         mapName = "嚎哭深渊"
                 to_append = mapName
@@ -2458,7 +2458,7 @@ def generate_LoLHistory_records_sgp(LoLHistory_data: dict[str, list[Any]], LoLGa
                         elif "mapskin_ha_crepe" in LoLGame_summary_json["gameModeMutators"]:
                             mapName = "进步之桥"
                         elif "mapskin_map12_jade" in LoLGame_summary_json["gameModeMutators"]:
-                            mapName = "LCU_Map12_Name_Jade"
+                            mapName = "召唤师峡谷？"
                         else:
                             mapName = "嚎哭深渊"
                     to_append = mapName
@@ -3580,7 +3580,7 @@ def generate_LoLGameSummary_records(LoLGame_summary_data: dict[str, list[Any]], 
         elif "mapskin_ha_crepe" in LoLGame_summary["gameModeMutators"]:
             mapName = "进步之桥"
         elif "mapskin_map12_jade" in LoLGame_summary["gameModeMutators"]:
-            mapName = "LCU_Map12_Name_Jade"
+            mapName = "召唤师峡谷？"
         else:
             mapName = "嚎哭深渊"
     #数据整理核心部分（Data organization core part）
@@ -4006,7 +4006,7 @@ def generate_LoLGameSummary_records_sgp(LoLGame_summary_data: dict[str, list[Any
         elif "mapskin_ha_crepe" in LoLGame_summary_json["gameModeMutators"]:
             mapName = "进步之桥"
         elif "mapskin_map12_jade" in LoLGame_summary_json["gameModeMutators"]:
-            mapName = "LCU_Map12_Name_Jade"
+            mapName = "召唤师峡谷？"
         else:
             mapName = "嚎哭深渊"
     #数据整理核心部分（Data organization core part）
